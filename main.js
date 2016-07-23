72a76d502c6c4da74e1c52be53cb5faa65270659
rizwanahmed19 <rizwan.ahmed19@outlook.com>
2016-07-24 01:22:16 +0500

    Add remove option

--- main.js
+++ main.js
@@ -1,14 +1,63 @@
 $(function(){
 	var $orders = $(".order-list");
+	var $name = $('#name');
+	var $order = $('#order');
+
+	function addOrder(order){
+		$orders.append('<li><button title="remove" class="remove" data-id=' + order.id +' >x</button><span>Name: </span>' + order.name +'<br /><span>Order: </span>' + order.order + '</li>');
+	}
 
 	$.ajax({
-		url: 'api/orders.json',
-		type: 'GET',
+		url: 'http://rest.learncode.academy/api/rizwanahmed/orders',
+		method: 'GET',
 		success: function(orders){
 			$.each(orders, function(index, order) {
-				$orders.append('<li><span>Name: </span>' + order.name +' <span>Order: </span>' + order.order + '</li>');
+				addOrder(order);			
 			});
+		},
+		error: function(){
+			alert('error loading data');
 		}
+	});
+
+	$('#add-order').on('click', function(event) {
+		/* Act on the event */
+		var orderToAdd = {
+			name: $name.val(),
+			order: $order.val()
+		};
+
+		$.ajax({
+			url: 'http://rest.learncode.academy/api/rizwanahmed/orders',
+			method: 'POST',
+			data: orderToAdd,
+			success: function(newOrder){
+				$name.val("");
+				$order.val("");
+			}
+			// error: function(){
+			// 	alert("error saving data");
+			// }
+		});
+	});
+
+	$orders.on('click', '.remove', function(event) {
+		$li = $(this).parent();
+		// alert($(this).attr('data-id'));
+
+		$.ajax({
+			url: 'http://rest.learncode.academy/api/rizwanahmed/orders/' + $(this).attr('data-id'),
+			type: 'DELETE',
+		})
+		.done(function() {
+			$li.fadeOut('300', function() {
+				$(this).remove();
+			});
+		})
+		.fail(function() {
+			alert("error removing data");
+		});
+		
 	});
 	
 });
